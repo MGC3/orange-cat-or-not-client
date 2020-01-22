7be9c8ad0e3975e0b6b6ad46bcc43caf5e5c53f3
michaelchun <michael.chun03@gmail.com>
2020-01-22 09:50:02 -0500

    Model successfully loads from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
 import './App.css';
 
 function App() {
+  const [model, setModel] = useState(null);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // load our model from the api and then store it in state
+    let model = tf
+      .loadLayersModel(
+        'https://aqueous-river-22133.herokuapp.com/model/model.json'
+      )
+      .then(data => {
+        setModel(data);
+        setMessage('Model successfully loaded');
+      })
+      .catch(err => console.warn('Unable to load model'));
+  }, []);
+
   return (
     <div className="app-container">
       <h1 className="title">Orange Cat or Not Orange Cat</h1>
+      <h3>{message}</h3>
     </div>
   );
 }
